7e08121fd1ea116b437fec6b948f8448b80f4938
JunkJumper <jose.srifi@gmail.com>
2020-09-23 11:22:28 +0200

    OOP + englishPrinter

--- src/textBlindTest/TranscriptSongParser.java
+++ src/textBlindTest/TranscriptSongParser.java
@@ -8,53 +8,89 @@ package textBlindTest;
 
 import java.io.FileReader;
 import java.util.List;
+import java.util.Random;
 import java.util.regex.Pattern;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 //import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.regex.*;
 
 public class TranscriptSongParser {
-	private static List<Song> listeQuotestoCheck = new ArrayList<Song>();
-	public static void readEnglish() throws IOException {
+	private List<Song> listeQuotestoCheck = new ArrayList<Song>();
+	
+	public TranscriptSongParser() {
+		this.setListeQuotestoCheck(new ArrayList<Song>());
+	}
+	
+	public static List<Song> readEnglish() throws IOException {
 		
 		Pattern pattern = Pattern.compile("^*@.+?@$"); //Regex to get all Strings of type "{any text}@{Song lyric}@"
 		Matcher matcher;
 		
 		BufferedReader in = new BufferedReader(new FileReader("./textFiles/quotes_base_unformatted.txt"));
 		String line; String[] o;
-		List<Song> listeQuotestoCheck = new ArrayList<Song>();
+		List<Song> listeQuotesChecked = new ArrayList<Song>();
 		while ((line = in.readLine()) != null)
 		{
 			o = line.split(";");
 			matcher = pattern.matcher(o[3]);
 			
 			if(matcher.find()) {
-				listeQuotestoCheck.add(new Song("english", o[3].substring(1, o[3].length()-1), Integer.parseInt(o[0].substring(1)), Integer.parseInt(o[1].substring(1))));
+				listeQuotesChecked.add(new Song("english", o[3].substring(1, o[3].length()-1), Integer.parseInt(o[0].substring(1)), Integer.parseInt(o[1].substring(1))));
 			}
 			
 		}
+		
 		//System.out.println(listeQuotestoCheck.toString());
 		
-		int i = 2;
-		while(i < 5) {
-			System.out.println(listeQuotestoCheck.get(i).toString());
+		/*
+		int i = (int)(Math.random()*listeQuotesChecked.size());
+		int max = i+5;
+		while(i < max) {
+			System.out.println(listeQuotesChecked.get(i).toString());
 			i++;
 		}
+		*/
 		
 		in.close();
+		return listeQuotesChecked;
 	}
 	
-	public static void writeEnglish() {
-		//PrintWriter writer = new PrintWriter("./textFiles/songs.txt", "UTF-8");
-		String s = null;
+	public static void writeEnglish(List<Song> l) throws IOException {
+		File fichier = new File("./textFiles/englishSongs.txt");
+		fichier.delete();
+		fichier.createNewFile();
+		PrintWriter writer = new PrintWriter("./textFiles/englishSongs.txt", "UTF-8");
 		
-		System.out.println();
+		String s = new String();
+		
+		for (Song song : l) {
+			s = "SongName;" + song.getLyric() + ";S" + song.getSeason() + ";E" + song.getEpisode();
+			writer.println(s);
+			System.out.println("Writing with format for lyric " + song.getLyric() + ".");
+		}
+		
+		
+		writer.close();
 	}
 	
 	public static void main(String[] args) throws IOException {
-		readEnglish();
+		TranscriptSongParser tsp = new TranscriptSongParser();
+		tsp.setListeQuotestoCheck(readEnglish());
+		writeEnglish(tsp.getListeQuotestoCheck());
+	}
+
+	public List<Song> getListeQuotestoCheck() {
+		return this.listeQuotestoCheck;
+	}
+
+	public void setListeQuotestoCheck(List<Song> listeQuotestoCheck) {
+		this.listeQuotestoCheck = listeQuotestoCheck;
 	}
 	
 	
