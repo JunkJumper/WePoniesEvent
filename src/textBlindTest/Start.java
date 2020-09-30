1f833c21ec5bc0ce05c0c0d4ee45ce08d5231cce
JunkJumper <jose.srifi@gmail.com>
2020-09-30 22:51:36 +0200

    simplify methods

--- src/textBlindTest/Start.java
+++ src/textBlindTest/Start.java
@@ -7,72 +7,61 @@ package textBlindTest;
  */
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-//import java.util.ArrayList;
 
 public class Start {
-	public static void main(String[] args) throws IOException, InterruptedException {
-		List<Song> library = new ArrayList<>();
-		TranscriptSongParser tsp = new TranscriptSongParser();
-		
-		/*
-		 * CALL IT ONLY ONE TIME !!!
-		 */
-					//initialise(tsp);
-		/*
-		 * CALL IT ONLY ONE TIME !!!
-		 */
-
-		tsp.setListeSongLines(TranscriptSongParser.fillList());
-		//System.out.println(tsp.toString());
-		
-		int i = 0;
-		String lesLyrics = "";
-		
-		do {
-			if(tsp.getListeSongLines().get(i).getSeason() == 99) {
-				break;
-			} else if(tsp.getListeSongLines().get(i).getName().equalsIgnoreCase(tsp.getListeSongLines().get(i+1).getName())) {
-				lesLyrics += tsp.getListeSongLines().get(i).getLyric() + "-a-";
-			} else {				//String n, String la, Lyrics l, int s, int e
-				library.add(new Song(tsp.getListeSongLines().get(i).getName(),
-									tsp.getListeSongLines().get(i).getLang(),
-									new Lyrics(lesLyrics),
-									tsp.getListeSongLines().get(i).getSeason(),
-									tsp.getListeSongLines().get(i).getEpisode()));
-				lesLyrics = "";
+	
+	private static class LocalGameManager {
+		@SuppressWarnings("unused")
+		public static void getRandomQuote(SongLibrary library) throws InterruptedException {
+			int j = (int)(Math.random()*library.getBibli().size()-1);
+			int maxLyricNumber = (int)(Math.random()*library.getBibli().get(j).getLyric().getL().size());
+			int lyricNumber = maxLyricNumber - 5;
+			
+			if(j <= 0) {
+				j += 5;
 			}
-			i++;
-		} while (i < tsp.getListeSongLines().size());
-		
-		//System.out.println(library.toString());
-		
-		
-		int j = (int)(Math.random()*library.size()-1);
-		int maxLyricNumber = (int)(Math.random()*library.get(j).getLyric().getL().size());
-		int lyricNumber = maxLyricNumber - 5;
-		
-		if(j <= 0) {
-			j += 5;
+			
+			if(maxLyricNumber <= 0) {
+				maxLyricNumber += 5;
+			}
+			
+			if(lyricNumber <= 0) {
+				lyricNumber += 5;
+			}
+			
+			while(lyricNumber < maxLyricNumber) {
+				System.out.println(library.getBibli().get(j).getLyric().getL().get(lyricNumber));
+				lyricNumber++;
+			}
+			Thread.sleep(8000);
+			System.out.println(library.getBibli().get(j).getName());
 		}
 		
-		if(maxLyricNumber <= 0) {
-			maxLyricNumber += 5;
+		@SuppressWarnings("unused")
+		public static void getSongFrom(SongLibrary library, int season, int episode) throws InterruptedException {
+			int i = -1;
+			for(int j = 0; j < library.getBibli().size(); j++) {
+				if (library.getBibli().get(j).getEpisode() == episode && library.getBibli().get(j).getSeason() == season) {
+					i = j;
+				}
+			}
+			if(i != -1) {
+				System.out.println(library.getBibli().get(i).toString());
+			}
 		}
 		
-		if(lyricNumber <= 0) {
-			lyricNumber += 5;
-		}
 		
-		while(lyricNumber < maxLyricNumber) {
-			System.out.println(library.get(j).getLyric().getL().get(lyricNumber));
-			lyricNumber++;
-		}
-		Thread.sleep(8000);
-		System.out.println(library.get(j).getName());
+	}
+	
+	public static void main(String[] args) throws IOException, InterruptedException {
 		
-		//System.out.println(library.get(1).toString());
+		SongLibrary bibli = new SongLibrary();
+		TranscriptSongParser tsp = new TranscriptSongParser();
+		tsp.setListeSongLines(TranscriptSongParser.fillList());
+		bibli.fillPlaylist(tsp);
+		LocalGameManager.getRandomQuote(bibli);
+		//LocalGameManager.getSongFrom(bibli, 8, 23);
+		
 		
 	}
 
