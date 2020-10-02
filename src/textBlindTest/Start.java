0fdebc2a2f45520e2a02f1828771c2110255f4fa
JunkJumper <jose.srifi@gmail.com>
2020-10-02 16:23:49 +0200

    add self controll of localGameManager

--- src/textBlindTest/Start.java
+++ src/textBlindTest/Start.java
@@ -9,59 +9,67 @@ package textBlindTest;
 import java.io.IOException;
 
 public class Start {
-	
-	private static final int QUOTES_NUMBER = 1;
-	
+
+	private static final int QUOTES_NUMBER = 2;
+
 	public static int alea(int nb) {
 		return (int) (Math.random() * nb);
 	}
-	
+
 	private static class LocalGameManager {
-		
+
 		@SuppressWarnings("unused")
 		public static void RandomQuote(SongLibrary library) throws InterruptedException {
 			LocalGameManager.RandomQuote(library, 5);
 		}
-		
+
 		public static void RandomQuote(SongLibrary library, int MAX) throws InterruptedException {
-			
-			int librarySize = library.getBibli().size()-1; //size of the song library 
-			int getRandomLyricIndex = Start.alea(librarySize); //return the index of a random Song from the library
-			int getSizeOfLyric = library.getBibli().get(getRandomLyricIndex).getLyric().getL().size()-1; //return the size of Lyrics String
-			int lastIndexOfLyric = Start.alea(getSizeOfLyric); // return the last index of th elyrci (ex 9)
-			if(lastIndexOfLyric <= MAX) {lastIndexOfLyric += MAX;}
+
+			int librarySize = library.getBibli().size() - 1; // size of the song library
+			int getRandomLyricIndex = Start.alea(librarySize); // return the index of a random Song from the library
+			int getSizeOfLyric = library.getBibli().get(getRandomLyricIndex).getLyric().getL().size() - 1; // return the
+																											// size of
+																											// Lyrics
+																											// String
+			int lastIndexOfLyric = Start.alea(getSizeOfLyric); // return the last index of the lyric (ex 9)
+			if (lastIndexOfLyric <= MAX) {
+				lastIndexOfLyric += MAX;
+			}
 			int currentRandomLyric = lastIndexOfLyric - MAX;
-			
-			while(currentRandomLyric < lastIndexOfLyric) {
-				System.out.println(library.getBibli().get(getRandomLyricIndex).getLyric().getL().get(currentRandomLyric));
+
+			while (currentRandomLyric < lastIndexOfLyric) {
+				System.out
+						.println(library.getBibli().get(getRandomLyricIndex).getLyric().getL().get(currentRandomLyric));
 				currentRandomLyric++;
 			}
+			System.out.println("======================");
 			Thread.sleep(8000);
 			System.out.println(library.getBibli().get(getRandomLyricIndex).getName());
 		}
-		
+
 		@SuppressWarnings("unused")
 		public static void SongFrom(SongLibrary library, int season, int episode) throws InterruptedException {
 			int i = -1;
-			for(int j = 0; j < library.getBibli().size(); j++) {
-				if (library.getBibli().get(j).getEpisode() == episode && library.getBibli().get(j).getSeason() == season) {
+			for (int j = 0; j < library.getBibli().size(); j++) {
+				if (library.getBibli().get(j).getEpisode() == episode
+						&& library.getBibli().get(j).getSeason() == season) {
 					i = j;
 				}
 			}
-			if(i != -1) {
+			if (i != -1) {
 				System.out.println(library.getBibli().get(i).toString());
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) throws IOException, InterruptedException {
-		
+
 		SongLibrary bibli = new SongLibrary();
 		TranscriptSongParser tsp = new TranscriptSongParser();
 		tsp.setListeSongLines(TranscriptSongParser.fillList());
 		bibli.fillPlaylist(tsp);
 		LocalGameManager.RandomQuote(bibli, QUOTES_NUMBER);
-		//LocalGameManager.SongFrom(bibli, 8, 23);
+		// LocalGameManager.SongFrom(bibli, 8, 23);
 	}
 
 }
