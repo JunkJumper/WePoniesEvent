e52effd8d54ec4819aea42778e7be2cb3211d401
JunkJumper <pauline.srifi@gmail.com>
2020-10-16 17:37:17 +0200

    #3 UID + Genetrate 1 question

--- www/game/getLyric.php
+++ www/game/getLyric.php
@@ -2,13 +2,31 @@
 /**
  *  ----------------------------------------------------------------------------
  * | @author     JunkJumper                                                     |
- * | @copyright  2029 - JunkJumper                                              |
+ * | @copyright  2020 - JunkJumper                                              |
  * | @license    https://creativecommons.org/licenses/by/4.0/  License CC BY 4.0|
  * | @since      File available since 10/08/2019                                |
  *  ----------------------------------------------------------------------------
  */
 
-$randomQuoteID = rand(1, 152);
+function charAt($str, $pos) {
+    return $str{$pos};
+}
+
+function generateUniqueID() : String {
+    $letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    $id = "";
+    
+    for($i = 0; $i < 8; $i++) {
+        if ($i < 4) {
+            $id .= charAt($letters, rand(0, 52));
+        } else {
+            $id .= rand(0,9);
+        }
+    }
+
+    return $id;
+}
 
 function getDifficulty($stringDiff) {
     $number = 0;
@@ -16,10 +34,19 @@ function getDifficulty($stringDiff) {
         case "e":
             $number = 5;
             break;
+        case "easy":
+                $number = 5;
+                break;
         case "m":
             $number = 3;
             break;
+        case "medium":
+            $number = 3;
+            break;
         case "h":
+            $number = 1;
+            break;
+        case "hard":
             $number = 1;
             break;
         default:
@@ -29,7 +56,8 @@ function getDifficulty($stringDiff) {
     return $number;
 }
 
-function getElement($int) {
+function getElementWithLyricTab() {
+    $int = rand(1, 152);
     include "database.php";
     $tabRet;
     try {
@@ -51,23 +79,41 @@ function getElement($int) {
     return $tabRet;
 }
 
+function RandomQuote($element, $MAX) {
+    $Lyric2return = "";
+    $Lyric2Tab = explode("-a-", $element[1]);
+    $getSizeOfLyric = sizeof($Lyric2Tab) - 1; // return the size of Lyrics String
+    $lastIndexOfLyric = rand(0,$getSizeOfLyric); // return the last index of the lyric (ex 9)
 
-$question = getElement($randomQuoteID);
+    if ($lastIndexOfLyric <= $MAX) {
+        $lastIndexOfLyric += $MAX;
+    }
+    $currentRandomLyric = $lastIndexOfLyric - $MAX;
 
-print_r($question);
-
-$tabLyrics = explode("-a-", $question[1]);
-$MAX = sizeof($tabLyrics);
-$currentI = rand(0,$MAX);
-
-if(($currentI - $MAX) <= 0) {
-    $currentI += $MAX;
+    while ($currentRandomLyric < $lastIndexOfLyric) {
+        if($currentRandomLyric >= $lastIndexOfLyric) {
+            
+        } else {
+            $Lyric2return .= $Lyric2Tab[$currentRandomLyric] .".";
+            $currentRandomLyric++;
+        }
+    }
+    return utf8_decode($Lyric2return);
 }
 
-echo $currentI;
+function fillRealQuestionTab(Array $initialArray, String $difficulty) {
+    $tabToFill = array();
+    $tabToFill[0] = $initialArray[0];                       //NameSong
+    $tabToFill[1] = RandomQuote($initialArray, $difficulty);//Lyric
+    $tabToFill[2] = $initialArray[2];                       //Season
+    $tabToFill[3] = $initialArray[3];                       //Episode
+    $tabToFill[4] = generateUniqueID();                         //Unique Question ID
 
-echo $tabLyrics[$currentI];
+    return $tabToFill;
+}
 
-$askLyric = "";
+$WriteLyric = fillRealQuestionTab(getElementWithLyricTab(), getDifficulty("easy"));
+
+print_r($WriteLyric);
 
 ?>
